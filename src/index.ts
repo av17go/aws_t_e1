e4d4929e22dbd8f648fa1154993765fdf6be6db6
Your Name <you@example.com>
2025-04-27 21:24:14 -0400

    aws stugg

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
 import express, { Request, Response } from 'express';
 
+const { MetadataService } = require("@aws-sdk/ec2-metadata-service");
+
+
 const app = express();
-const port = 3000;
+const port = 80;
+const hostname = '0.0.0.0'; // Set the hostname (e.g., 0.0.0.0 to listen on all interfaces)
+
+const metadataService = new MetadataService({});
+
+
+const getRegion = async () => {
+    try {
+        const region = await metadataService.request("/latest/meta-data/placement/availability-zone", {});
+        
+        return region;
+    } catch (error) {
+        console.error('Error fetching regions:', error);
+        throw error;
+    }
+};
 
 // Define a GET endpoint
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+app.get('/', async (req: Request, res: Response) => {
+    const r = await getRegion();
+    // res.send('Hello, World!');
+    res.send(`AWS Region: ${JSON.stringify(r, null, 1)}`);
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, hostname, () => {
+    console.log(`Server is running on http://${hostname}:${port}`);
 });
+
+
+
+
+
